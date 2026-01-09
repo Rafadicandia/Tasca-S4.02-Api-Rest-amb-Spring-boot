9e63ade032745df3e87a4ab3c4a8af1616cd47ed
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2026-01-09 16:21:07 +0100

    test: add test shouldReturnFruitWhenIdExist

--- fruit-api-h2/src/test/java/cat/itacademy/s04/s02/n01/fruit/controllers/FruitControllerTest.java
+++ fruit-api-h2/src/test/java/cat/itacademy/s04/s02/n01/fruit/controllers/FruitControllerTest.java
@@ -1,4 +1,5 @@
 package cat.itacademy.s04.s02.n01.fruit.controllers;
+
 import cat.itacademy.s04.s02.n01.fruit.model.FruitDTO;
 import cat.itacademy.s04.s02.n01.fruit.model.FruitResponseDTO;
 import cat.itacademy.s04.s02.n01.fruit.services.FruitService;
@@ -7,13 +8,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import org.springframework.http.MediaType;
-import static org.mockito.Mockito.*;
 import org.springframework.test.web.servlet.MockMvc;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 @WebMvcTest(FruitController.class)
 class FruitControllerTest {
@@ -44,6 +46,19 @@ class FruitControllerTest {
                 .andExpect(jsonPath("$.weightInKilos").value(5));
 
         verify(fruitService, times(1)).save(any(FruitDTO.class));
+
+    }
+
+    @Test
+    void shouldReturnFruitWhenIdExist() throws Exception {
+        Long fruitId = 21L;
+        FruitResponseDTO mockResponse = new FruitResponseDTO(fruitId, "Orange", 3);
+        when(fruitService.getById(fruitId)).thenReturn(mockResponse);
+
+        mockMvc.perform(get("/fruits/{id}", fruitId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(fruitId))
+                .andExpect(jsonPath("$.name").value("Orange"));
 
     }
 
