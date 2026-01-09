5dda7816ea1a19137d7f304d73b6b98b2aec2a3b
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2026-01-09 15:22:09 +0100

    refactor: adapt controller for DTO response

--- fruit-api-h2/src/main/java/cat/itacademy/s04/s02/n01/fruit/controllers/FruitController.java
+++ fruit-api-h2/src/main/java/cat/itacademy/s04/s02/n01/fruit/controllers/FruitController.java
@@ -2,8 +2,10 @@ package cat.itacademy.s04.s02.n01.fruit.controllers;
 
 import cat.itacademy.s04.s02.n01.fruit.model.Fruit;
 import cat.itacademy.s04.s02.n01.fruit.model.FruitDTO;
+import cat.itacademy.s04.s02.n01.fruit.model.FruitResponseDTO;
 import cat.itacademy.s04.s02.n01.fruit.services.FruitService;
 import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,21 +13,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@AllArgsConstructor
 @RestController
 @RequestMapping("/fruits")
 public class FruitController {
 
     private final FruitService service;
 
-    public FruitController(FruitService service) {
-        this.service = service;
-    }
-
     @PostMapping
-    public ResponseEntity<FruitDTO> createFruit(
+    public ResponseEntity<FruitResponseDTO> createFruit(
             @Valid @RequestBody FruitDTO fruitRequest) {
 
-        FruitDTO fruitResponse = service.save(fruitRequest);
+        FruitResponseDTO fruitResponse = service.save(fruitRequest);
 
         return ResponseEntity
                 .status(HttpStatus.CREATED)
