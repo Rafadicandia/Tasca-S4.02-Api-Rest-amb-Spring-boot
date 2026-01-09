c6bb1d26f229687717dc953bf54750bc6399ddea
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2026-01-09 13:38:08 +0100

    feat: add Fruit interface

---
+++ fruit-api-h2/src/main/java/cat/itacademy/s04/s02/n01/fruit/services/FruitService.java
@@ -0,0 +1,8 @@
+package cat.itacademy.s04.s02.n01.fruit.services;
+
+import cat.itacademy.s04.s02.n01.fruit.model.Fruit;
+import cat.itacademy.s04.s02.n01.fruit.model.FruitDTO;
+
+public interface FruitService {
+    public FruitDTO save(FruitDTO userRequestNewFruit);
+}
