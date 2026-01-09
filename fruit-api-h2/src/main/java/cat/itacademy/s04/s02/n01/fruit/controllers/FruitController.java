529b9838263b80f1ad3353284281413b9c629949
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2026-01-09 14:00:32 +0100

    feat: add postMapping to controller with createFruit method

--- fruit-api-h2/src/main/java/cat/itacademy/s04/s02/n01/fruit/controllers/FruitController.java
+++ fruit-api-h2/src/main/java/cat/itacademy/s04/s02/n01/fruit/controllers/FruitController.java
@@ -1,8 +1,35 @@
 package cat.itacademy.s04.s02.n01.fruit.controllers;
 
+import cat.itacademy.s04.s02.n01.fruit.model.Fruit;
+import cat.itacademy.s04.s02.n01.fruit.model.FruitDTO;
+import cat.itacademy.s04.s02.n01.fruit.services.FruitService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/fruits")
 public class FruitController {
+
+    private final FruitService service;
+
+    public FruitController(FruitService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<FruitDTO> createFruit(
+            @Valid @RequestBody FruitDTO fruitRequest) {
+
+        FruitDTO fruitResponse = service.save(fruitRequest);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(fruitResponse);
+    }
 
 }
