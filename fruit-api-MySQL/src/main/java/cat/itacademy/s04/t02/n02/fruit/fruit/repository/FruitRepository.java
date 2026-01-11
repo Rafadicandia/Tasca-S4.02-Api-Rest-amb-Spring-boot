f56733e6ef3d3d7ae3922bd7db37610731afde8d
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2026-01-11 17:22:25 +0100

    Add method to find fruits by provider ID
    Introduced a new repository method findByProviderId in FruitRepository to retrieve fruits associated with a specific provider.

--- fruit-api-MySQL/src/main/java/cat/itacademy/s04/t02/n02/fruit/fruit/repository/FruitRepository.java
+++ fruit-api-MySQL/src/main/java/cat/itacademy/s04/t02/n02/fruit/fruit/repository/FruitRepository.java
@@ -4,5 +4,8 @@ package cat.itacademy.s04.t02.n02.fruit.fruit.repository;
 import cat.itacademy.s04.t02.n02.fruit.fruit.model.Fruit;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FruitRepository extends JpaRepository<Fruit, Long> {
+    List<Fruit> findByProviderId(Long providerId);
 }
