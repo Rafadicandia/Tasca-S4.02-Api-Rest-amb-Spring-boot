7938336254bf7c489ce3c6e838d11f1e845d368c
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2026-01-11 13:29:01 +0100

    feat: Add POST endpoint to create provider
    Introduced a POST mapping in ProviderController to handle creation of providers using ProviderDTO. The endpoint returns a ProviderResponseDTO with HTTP status 201 (Created).

--- fruit-api-MySQL/src/main/java/cat/itacademy/s04/t02/n02/fruit/provider/controller/ProviderController.java
+++ fruit-api-MySQL/src/main/java/cat/itacademy/s04/t02/n02/fruit/provider/controller/ProviderController.java
@@ -1,7 +1,13 @@
 package cat.itacademy.s04.t02.n02.fruit.provider.controller;
 
-import cat.itacademy.s04.t02.n02.fruit.provider.service.ProviderService;
+import cat.itacademy.s04.t02.n02.fruit.provider.model.ProviderDTO;
+import cat.itacademy.s04.t02.n02.fruit.provider.model.ProviderResponseDTO;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +17,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProviderController {
 
     private final ProviderService service;
+
+    @PostMapping
+    public ResponseEntity<ProviderResponseDTO> create(@Valid @RequestBody ProviderDTO providerDTO) {
+        ProviderResponseDTO response = service.create(providerDTO);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
 }
