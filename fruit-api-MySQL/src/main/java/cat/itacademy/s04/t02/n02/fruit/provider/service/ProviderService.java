65759921644c3dde2f4c67ed56e053f4bf62819c
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2026-01-11 13:28:36 +0100

    Convert ProviderService to interface and add create method
    ProviderService is now an interface instead of a class, and a create method accepting ProviderDTO and returning ProviderResponseDTO has been added. This prepares the service layer for implementation and enforces a contract for provider creation.

--- fruit-api-MySQL/src/main/java/cat/itacademy/s04/t02/n02/fruit/provider/service/ProviderService.java
+++ fruit-api-MySQL/src/main/java/cat/itacademy/s04/t02/n02/fruit/provider/service/ProviderService.java
@@ -1,4 +1,8 @@
 package cat.itacademy.s04.t02.n02.fruit.provider.service;
 
-public class ProviderService {
+import cat.itacademy.s04.t02.n02.fruit.provider.model.ProviderDTO;
+import cat.itacademy.s04.t02.n02.fruit.provider.model.ProviderResponseDTO;
+
+public interface ProviderService {
+    public ProviderResponseDTO create(ProviderDTO providerDTO);
 }
