38c13eeed3b5b3787e25d023a523a9ac4a2c92b9
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2026-01-11 19:18:44 +0100

    Add custom exceptions for provider errors
    Introduced ProviderAlreadyExistsException and ProviderHasFruitsException to handle specific provider-related error cases in the application.

---
+++ fruit-api-MySQL/src/main/java/cat/itacademy/s04/t02/n02/fruit/provider/exception/ProviderAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package cat.itacademy.s04.t02.n02.fruit.provider.exception;
+
+public class ProviderAlreadyExistsException extends RuntimeException {
+    public ProviderAlreadyExistsException(String message) {
+        super(message);
+    }
+}
