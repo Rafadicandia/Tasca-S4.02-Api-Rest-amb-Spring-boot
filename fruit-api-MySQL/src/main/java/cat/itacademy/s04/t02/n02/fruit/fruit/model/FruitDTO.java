068241ff0a2523f774ce81112b4db1ebd12e803e
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2026-01-11 12:48:01 +0100

    Add providerId field to FruitDTO record
    Introduces a new providerId field to the FruitDTO record with a @NotNull validation constraint, ensuring that each fruit entry is associated with a provider.

--- fruit-api-MySQL/src/main/java/cat/itacademy/s04/t02/n02/fruit/fruit/model/FruitDTO.java
+++ fruit-api-MySQL/src/main/java/cat/itacademy/s04/t02/n02/fruit/fruit/model/FruitDTO.java
@@ -1,11 +1,13 @@
 package cat.itacademy.s04.t02.n02.fruit.fruit.model;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 
 public record FruitDTO(
         @NotBlank(message = "Name can't be blank") String name,
-        @Positive(message = "Weight must be positive") int weightInKilos) {
+        @Positive(message = "Weight must be positive") int weightInKilos,
+        @NotNull(message = "Provider ID is required") Long providerId) {
 
 }
