72c914021fc8a476124fe5fadb9b65a794ba32b9
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2026-01-11 19:21:06 +0100

    feat: add existsByNameAndIdNot method to Provider respository

--- fruit-api-MySQL/src/main/java/cat/itacademy/s04/t02/n02/fruit/provider/repository/ProviderRepository.java
+++ fruit-api-MySQL/src/main/java/cat/itacademy/s04/t02/n02/fruit/provider/repository/ProviderRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProviderRepository extends JpaRepository<Provider, Long> {
     boolean existsByName(String name);
+    boolean existsByNameAndIdNot(String name, Long id);
 }
