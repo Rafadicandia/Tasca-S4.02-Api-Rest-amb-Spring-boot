0fd58b257e90fa64e20e2ff75ae0ff8ee02c8ada
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2026-01-12 11:05:49 +0100

    feat: create OrderServiceImpl class

---
+++ fruit-order-api/src/main/java/cat/itacademy/s04/t02/n03/fruit/service/OrderServiceImpl.java
@@ -0,0 +1,33 @@
+package cat.itacademy.s04.t02.n03.fruit.service;
+
+import cat.itacademy.s04.t02.n03.fruit.model.OrderDTO;
+import cat.itacademy.s04.t02.n03.fruit.model.OrderResponseDTO;
+
+import java.util.List;
+
+public class OrderServiceImpl implements OrderService{
+    @Override
+    public OrderResponseDTO createOrder(OrderDTO order) {
+        return null;
+    }
+
+    @Override
+    public List<OrderResponseDTO> getAllOrders() {
+        return null;
+    }
+
+    @Override
+    public OrderResponseDTO getOrderById(String id) {
+        return null;
+    }
+
+    @Override
+    public OrderResponseDTO updateOrder(String id, OrderDTO order) {
+        return null;
+    }
+
+    @Override
+    public void deleteOrder(String id) {
+
+    }
+}
