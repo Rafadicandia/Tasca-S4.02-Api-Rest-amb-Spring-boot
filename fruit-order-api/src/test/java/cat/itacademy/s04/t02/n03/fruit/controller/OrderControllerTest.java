15216a9ec9c4f2330b4e4383a9972eb378a46198
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2026-01-12 12:30:39 +0100

    test: add shouldCreateOrderSuccessfully() for red test stage

--- fruit-order-api/src/test/java/cat/itacademy/s04/t02/n03/fruit/controller/OrderControllerTest.java
+++ fruit-order-api/src/test/java/cat/itacademy/s04/t02/n03/fruit/controller/OrderControllerTest.java
@@ -1,14 +1,25 @@
 package cat.itacademy.s04.t02.n03.fruit.controller;
 
+import cat.itacademy.s04.t02.n03.fruit.model.OrderDTO;
+import cat.itacademy.s04.t02.n03.fruit.model.OrderItem;
+import cat.itacademy.s04.t02.n03.fruit.model.OrderResponseDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.skyscreamer.jsonassert.JSONAssert.assertEquals;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Testcontainers
@@ -27,14 +38,16 @@ class OrderControllerTest {
 
     @Test
     void shouldCreateOrderSuccessfully() {
-        // GIVEN: Un OrderDTO válido (recuerda usar una fecha futura)
-        // OrderDTO request = new OrderDTO("Juan", LocalDate.now().plusDays(1), List.of(...));
 
-        // WHEN: Hacemos el POST
-        // ResponseEntity<OrderResponseDTO> response = restTemplate.postForEntity("/orders", request, OrderResponseDTO.class);
+        OrderItem item = new OrderItem("Manzana", 10);
+        OrderDTO request = new OrderDTO("Cliente Test", LocalDate.now().plusDays(2), List.of(item));
 
-        // THEN: Verificamos el status 201
-        // assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        ResponseEntity<OrderResponseDTO> response = restTemplate.postForEntity("/orders", request, OrderResponseDTO.class);
+
+
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        assertNotNull(response.getBody().id());
+        assertEquals("Cliente Test", response.getBody().clientName());
     }
 
 }
