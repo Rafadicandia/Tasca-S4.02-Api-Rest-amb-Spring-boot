94d3526dc4c4f34db9f1fd5e731e8038dfec9086
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2026-01-12 13:21:14 +0100

    Feat: add logic to methods in Service after testing

--- fruit-order-api/src/main/java/cat/itacademy/s04/t02/n03/fruit/service/OrderServiceImpl.java
+++ fruit-order-api/src/main/java/cat/itacademy/s04/t02/n03/fruit/service/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package cat.itacademy.s04.t02.n03.fruit.service;
 
+import cat.itacademy.s04.t02.n03.fruit.exception.OrderNotFoundException;
 import cat.itacademy.s04.t02.n03.fruit.model.Order;
 import cat.itacademy.s04.t02.n03.fruit.model.OrderDTO;
 import cat.itacademy.s04.t02.n03.fruit.model.OrderResponseDTO;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Validated
 @AllArgsConstructor
@@ -23,27 +25,44 @@ public class OrderServiceImpl implements OrderService {
 
         Order savedOrder = orderRepository.save(order);
 
-        return mapToResponseDTO(savedOrder);
+        if (savedOrder == null){
+            throw new RuntimeException("Failed to save order in Mongodb");
+        }
+
+            return mapToResponseDTO(savedOrder);
     }
 
     @Override
     public List<OrderResponseDTO> getAllOrders() {
-        return null;
+        return orderRepository.findAll().stream()
+                .map(this::mapToResponseDTO)
+                .collect(Collectors.toList());
     }
 
     @Override
     public OrderResponseDTO getOrderById(String id) {
-        return null;
+        return orderRepository.findById(id)
+                .map(this::mapToResponseDTO)
+                .orElseThrow(() -> new OrderNotFoundException("Order not found with id: " + id));
     }
 
     @Override
-    public OrderResponseDTO updateOrder(String id, OrderDTO order) {
-        return null;
+    public OrderResponseDTO updateOrder(String id, OrderDTO orderDTO) {
+        return orderRepository.findById(id)
+                .map(existingOrder -> {
+                    Order toUpdate = mapToEntity(orderDTO);
+                    toUpdate.setId(id);
+                    return mapToResponseDTO(orderRepository.save(toUpdate));
+                })
+                .orElseThrow(() -> new OrderNotFoundException("Order not found with id: " + id));
     }
 
     @Override
     public void deleteOrder(String id) {
-
+        if (!orderRepository.existsById(id)) {
+            throw new OrderNotFoundException("Order not found with id: " + id);
+        }
+        orderRepository.deleteById(id);
     }
 
     private Order mapToEntity(OrderDTO dto) {
