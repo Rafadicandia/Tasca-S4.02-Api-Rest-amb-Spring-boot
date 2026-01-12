da3b99b0270669c33756fd10323f76a6a00c4b65
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2026-01-12 11:04:16 +0100

    feat: create OrderDTO class for data transport

---
+++ fruit-order-api/src/main/java/cat/itacademy/s04/t02/n03/fruit/model/OrderDTO.java
@@ -0,0 +1,20 @@
+package cat.itacademy.s04.t02.n03.fruit.model;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public record OrderDTO(
+        @NotBlank(message = "Name can't be blank")
+        String clientName,
+        @Future(message = "Delivery date must be issued for tomorrow or later")
+        LocalDate deliveryDate,
+        @NotEmpty(message = "The order must have items in it")
+        @Valid
+        List<OrderItem> items
+) {
+}
