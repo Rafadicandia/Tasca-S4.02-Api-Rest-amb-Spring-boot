641b567898eb5fea93866500c6084bbb0d6f0394
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2026-01-12 11:04:53 +0100

    feat: create OrderResponseDTO for data transport

---
+++ fruit-order-api/src/main/java/cat/itacademy/s04/t02/n03/fruit/model/OrderResponseDTO.java
@@ -0,0 +1,15 @@
+package cat.itacademy.s04.t02.n03.fruit.model;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public record OrderResponseDTO(
+        String id,
+
+        String clientName,
+
+        LocalDate deliveryDate,
+
+        List<OrderItem> items
+) {
+}
