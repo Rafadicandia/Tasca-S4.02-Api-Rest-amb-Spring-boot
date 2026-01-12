aeb3b26b5a46ecebeec35a8ab0888d33928cde0e
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2026-01-12 11:33:47 +0100

    feat: add OrderController class

---
+++ fruit-order-api/src/main/java/cat/itacademy/s04/t02/n03/fruit/controller/OrderController.java
@@ -0,0 +1,55 @@
+package cat.itacademy.s04.t02.n03.fruit.controller;
+
+import cat.itacademy.s04.t02.n03.fruit.model.Order;
+import cat.itacademy.s04.t02.n03.fruit.model.OrderDTO;
+import cat.itacademy.s04.t02.n03.fruit.model.OrderResponseDTO;
+import cat.itacademy.s04.t02.n03.fruit.service.OrderService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+    private final OrderService orderService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public OrderResponseDTO createOrder(@Valid @RequestBody OrderDTO orderDTO) {
+
+        return orderService.createOrder(orderDTO);
+    }
+
+    @GetMapping
+    public List<OrderResponseDTO> getAllOrders() {
+        return orderService.getAllOrders();
+    }
+
+    @GetMapping("/{id}")
+    public OrderResponseDTO getOrderById(@PathVariable String id) {
+        return orderService.getOrderById(id);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<OrderResponseDTO> updateFruit(
+            @PathVariable String id,
+            @Valid @RequestBody OrderDTO orderRequest) {
+
+        OrderResponseDTO providerResponse = orderService.updateOrder(id, orderRequest);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(providerResponse);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteOrder(@PathVariable String id) {
+        orderService.deleteOrder(id);
+    }
+}
